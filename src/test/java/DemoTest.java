d84f393a83e4897fea8be2fa108cacefcc184449
rohithtadi <tssrohith1@gmail.com>
2022-03-11 14:35:50 +0100

    hi

--- src/test/java/DemoTest.java
+++ src/test/java/DemoTest.java
@@ -6,6 +6,6 @@ public class DemoTest {
     @Test
     public void test1(){
         Demo a = new Demo();
-        assertEquals("it is a prime number",a.Prime(3));
+        assertEquals("it is a prime number",a.Prime(2));
     }
 }
